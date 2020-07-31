3994bf8f6d9ca34498511808a2879db7de6eea19
Kuba <kubahermyt@gmail.com>
2020-07-31 11:49:38 +0200

    feat: local storage added

--- src/context.js
+++ src/context.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 
 export const AppContext = React.createContext();
 
 const TaskList = ({ children }) => {
-  const [tasks, setTasks] = useState([
+  const initialState = JSON.parse(localStorage.getItem(`tasks`)) || [
     { title: "Learn React", id: 1 },
     { title: "Do groceries", id: 2 },
     { title: "Cleanup Apartament", id: 3 },
-  ]);
+  ];
+
+  const [tasks, setTasks] = useState(initialState);
+
+  useEffect(() => {
+    localStorage.setItem(`tasks`, JSON.stringify(tasks));
+  }, [tasks]);
 
   const [editItem, setEditItem] = useState(null);
 
