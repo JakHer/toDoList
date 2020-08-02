9be0c67c466c54cc38b5a3f2549973e07c60a033
Kuba <kubahermyt@gmail.com>
2020-08-02 09:51:42 +0200

    Updates

---
+++ precache-manifest.b114e9c8ea0931ccb0d055e23b45ad79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3fe869a7e903c8b5f6313f2f1483fd",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "5e9e88553af166e502d4",
+    "url": "/toDoList/static/css/main.bddd9b59.chunk.css"
+  },
+  {
+    "revision": "38a365cc25e49f67a1b8",
+    "url": "/toDoList/static/js/2.483c46aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoList/static/js/2.483c46aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9e88553af166e502d4",
+    "url": "/toDoList/static/js/main.31474039.chunk.js"
+  },
+  {
+    "revision": "66a223b8a2c5ac88bd37",
+    "url": "/toDoList/static/js/runtime-main.baa4d591.js"
+  }
+]);
